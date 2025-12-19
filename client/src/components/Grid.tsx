c185ad8d518087b28aa5f92f4d9d9bc69d440321
Harivansh <jazzz1815@gmail.com>
2025-12-19 21:01:50 +0530

    adding layout

--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from "react";
 
 function Grid() {
+  //   const [box, setBox] = useState([9]);
+  const [visible, setVisible] = useState(false);
   return (
-    <div>Grid</div>
-  )
+    <div className="flex h-screen w-full justify-center items-center bg-black">
+      <div className="grid  grid-cols-4 gap-2 p-2 bg-white  ">
+        {Array.from({ length: 16 }).map((_, index) => (
+          <button
+            onClick={(e) => console.log(index + 1)}
+            key={index}
+            className={
+              visible
+                ? "w-22 h-22  bg-purple-500 rounded-lg "
+                : "w-22 h-22  bg-purple-400 rounded-lg animate-pulse"
+            }
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Grid
+export default Grid;
